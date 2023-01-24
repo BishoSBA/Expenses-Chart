56c7ec40df454b757d85328c657dd9ab17af77c3
Beshir Saeed <Beshir-saeed@live.com>
2023-01-24 19:01:25 +0000

    Improve Chart tooltip

--- script.js
+++ script.js
@@ -2,51 +2,61 @@ const ctx = document.getElementById('myChart');
 
 const data = fetch("./data.json")
   .then((response) => response.json())
+  .then(data => {
+    const days = data.map(x => x.day)
+    const amounts = data.map(x => x.amount)
 
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-    datasets: [{
 
-      data: [12, 39, 3, 5, 2, 3, 2],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    hoverBackgroundColor: "hsl(186, 34%, 60%)",
-    backgroundColor: "hsl(10, 79%, 65%)",
-    borderRadius: 2,
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        grid: {
-          display: false
-        },
-        border: {
-          display: false
-        }
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: days,
+        datasets: [{
+
+          data: amounts,
+          borderWidth: 1
+        }]
       },
-      y: {
-        ticks: {
-          callback: function(value) {
-            return '$' + value;
+      options: {
+        hoverBackgroundColor: "hsl(186, 34%, 60%)",
+        backgroundColor: "hsl(10, 79%, 65%)",
+        borderRadius: 2,
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            grid: {
+              display: false
+            },
+            border: {
+              display: false
+            }
           },
-          display: false,
+          y: {
+            ticks: {
+              display: false,
+            },
+            grid: {
+              display: false
+            },
+            border: {
+              display: false
+            }
+          }
         },
-        grid: {
-          display: false
-        },
-        border: {
-          display: false
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            displayColors: false,
+            callbacks: {
+              title: (value) => '',
+              label: (value) => '$' + value.parsed.y,
+            }
+          },
         }
       }
-    },
-    plugins: {
-      legend: {
-        display: false
-      }
-    }
-  }
-});
+    });
+  });
+
